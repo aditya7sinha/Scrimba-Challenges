2c4d6f0ebc1910cdd42ddd2f169e5ca07f64fa12
adisin7 <1706005@kiit.ac.in>
2020-10-17 13:45:02 +0530

    Deployement projects

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+PART 2: STRETCH GOAL
+
+1. Validate that it's an email the user has entered
+2. Give feedback as to whether the form was successfully submitted or not
+3. Go crazy with the error/success feedback (e.g. animations, sounds)
+
+*/
+
+const email = document.getElementById("email-input")
+const form = document.getElementById("myForm")
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log("email: ", email.value)
+    if(validateEmail(email.value))
+    {
+        console.log("Valid Email")
+    }
+    else{
+        console.log("Not a valid email")
+    }
+})
+
